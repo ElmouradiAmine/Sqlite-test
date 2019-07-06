67f2100e6e645d0f8c70951b5e36bddd9bf656f2
Amine EL MOURADI <elmouradi.amine98@gmail.com>
2019-07-06 19:35:12 +0100

    First commit

---
+++ SqliteDemo/Form1.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Data.SQLite
+Imports System.Runtime.InteropServices
+
+Public Class Form1
+    Dim DB_Path As String
+    Dim TableName As String = "hello"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DB_Path = "Data Source = hello.db"
+        Dim SQLiteCon As New SQLiteConnection(DB_Path)
+        Try
+            SQLiteCon.Open()
+            MsgBox("connceted")
+        Catch ex As Exception
+            SQLiteCon.Dispose()
+            SQLiteCon = Nothing
+            MsgBox(ex.Message)
+
+        End Try
+
+    End Sub
+End Class
---
+++ SqliteDemo/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SqliteDemo")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP Inc.")>
+<Assembly: AssemblyProduct("SqliteDemo")>
+<Assembly: AssemblyCopyright("Copyright © HP Inc. 2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("80f48c64-e5cb-424f-bb6e-bc106c9a0b9b")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
